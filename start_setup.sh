c4328a4a18020bcf9e48da2b154a5108ce98c565
Mukitul Islam Mukit <imukit007@gmail.com>
2017-10-28 02:56:12 +0600

    Add command: upgrade && build essential

--- start_setup.sh
+++ start_setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # JavaScript Development Environment Setup on Ubuntu
 # Author : MI MUKIT
+
+# Set sudo throughout terminal session
+sudo -s
+
+# System update && upgrade
+sudo apt update
+
+sudo apt upgrade
+
+# Install build essentials
+sudo apt install -y build-essential libssl-dev
+
