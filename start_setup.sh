e4cefd6de32604b7940cac057fce7165dafbe40e
MI MUKIT <hello@mukit.me>
2017-10-28 04:11:38 +0600

    Add command: 'Basic Setup' group

--- start_setup.sh
+++ start_setup.sh
@@ -5,11 +5,25 @@
 # Set sudo throughout terminal session
 sudo -s
 
+# Basic Setup
+#============
+
+# Add essential repositories
+sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:noobslab/macbuntu
+
 # System update && upgrade
 sudo apt update
-
 sudo apt upgrade
 
 # Install build essentials
-sudo apt install -y build-essential libssl-dev
+sudo apt install -y gcc build-essential libssl-dev
 
+# Package installer for .deb
+sudo apt install -y gdebi
+
+# HTTP request tool curl
+sudo apt install -y curl
+
+# Add 'New Document' on context menu
+touch ~/Templates/Untitled\ Document
